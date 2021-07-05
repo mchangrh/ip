01f716ea854aa021c893580c7429771ea94e5290
Michael C <michael@mchang.name>
2021-07-05 03:57:43 -0400

    improve performance of IP

--- src/ip.js
+++ src/ip.js
@@ -11,34 +11,27 @@ const handleRequest = (request) => {
   // more options here https://developers.cloudflare.com/workers/runtime-apis/request#incomingrequestcfproperties
   const country = (request.cf || {}).country
   // return json object if requested
-  if (request.url.endsWith(".json")) return jsonResponse({ip, country})
-  // return text response includes api path
-  return (request.url.includes("/api")) ? textResponse(ip) : htmlResponse(ip)
+  if (request.url.endsWith(".json")) return new Response(JSON.stringify({ip, country}), {
+    headers: {
+      ...noCache,
+      "Content-Type": "application/json;charset=UTF-8"
+    }})
+  // return text response if on API path, otherwise HTML
+  return (request.url.includes("/api"))
+    ? new Response(ip, {
+      headers: {
+        ...noCache,
+        "Content-Type": "text/plain;charset=UTF-8",
+      }})
+    : new Response(`<!DOCTYPE html><style>body{background-color:#121212;color:#D5D7D8;font-size:7vmin;display:flex;min-height:calc(100vh - 3.25rem);justify-content:center;align-items:center}</style><pre>${ip}`, {
+      headers: {
+        ...noCache,
+        "Content-Type": "text/html;charset=UTF-8"
+      }
+    })
 }
 
 const noCache = {
   "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
   "Expires": "0",
 }
-
-const jsonResponse = (res) => new Response(JSON.stringify(res), {
-  headers: {
-    ...noCache,
-    "Content-Type": "application/json;charset=UTF-8"
-  }
-})
-
-const textResponse = (str) => new Response(str, {
-  headers: {
-    ...noCache,
-    "Content-Type": "text/plain;charset=UTF-8",
-  }
-});
-
-const htmlResponse = (ip) =>
-new Response(`<!DOCTYPE html><style>body{background-color:#121212;color:#D5D7D8;font-size:7vmin;display:flex;min-height:calc(100vh - 3.25rem);justify-content:center;align-items:center}</style><pre>${ip}`, {
-  headers: {
-    ...noCache,
-    "Content-Type": "text/html;charset=UTF-8"
-  }
-})
