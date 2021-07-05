1461cdcf9a0295953ac36fe619900755b36921c9
Michael C <michael@mchang.name>
2021-07-05 03:47:55 -0400

    remove submodule, use mkdir

--- src/ip.js
+++ src/ip.js
@@ -16,21 +16,21 @@ const handleRequest = (request) => {
   return (request.url.includes("/api")) ? textResponse(ip) : htmlResponse(ip)
 }
 
-const defaultHeaders = {
+const noCache = {
   "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
   "Expires": "0",
 }
 
 const jsonResponse = (res) => new Response(JSON.stringify(res), {
   headers: {
-    ...defaultHeaders,
+    ...noCache,
     "Content-Type": "application/json;charset=UTF-8"
   }
 })
 
 const textResponse = (str) => new Response(str, {
   headers: {
-    ...defaultHeaders,
+    ...noCache,
     "Content-Type": "text/plain;charset=UTF-8",
   }
 });
@@ -38,7 +38,7 @@ const textResponse = (str) => new Response(str, {
 const htmlResponse = (ip) =>
 new Response(`<!DOCTYPE html><style>body{background-color:#121212;color:#D5D7D8;font-size:7vmin;display:flex;min-height:calc(100vh - 3.25rem);justify-content:center;align-items:center}</style><pre>${ip}`, {
   headers: {
-    ...defaultHeaders,
+    ...noCache,
     "Content-Type": "text/html;charset=UTF-8"
   }
 })
--- src/vpn.js
+++ src/vpn.js
@@ -23,21 +23,21 @@ const handleRequest = (request) => {
   return sendResponse('Not Connected')
 }
 
-const defaultHeaders = {
+const noCache = {
   "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
   "Expires": "0",
 }
 
 const jsonResponse = (res) => new Response(JSON.stringify(res), {
   headers: {
-    ...defaultHeaders,
+    ...noCache,
     "Content-Type": "application/json;charset=UTF-8"
   }
 })
 
 const textResponse = (str) => new Response(str, {
   headers: {
-    ...defaultHeaders,
+    ...noCache,
     "Content-Type": "text/plain;charset=UTF-8",
   }
 });
@@ -45,7 +45,7 @@ const textResponse = (str) => new Response(str, {
 const htmlResponse = (ip) =>
 new Response(`<!DOCTYPE html><style>body{background-color:#121212;color:#D5D7D8;font-size:7vmin;display:flex;min-height:calc(100vh - 3.25rem);justify-content:center;align-items:center}</style><pre>${ip}`, {
   headers: {
-    ...defaultHeaders,
+    ...noCache,
     "Content-Type": "text/html;charset=UTF-8"
   }
 })
