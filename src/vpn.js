d6b602dc6c1fa3a4855c2f02d623999cbee3375e
Michael C <michael@mchang.name>
2021-12-16 18:41:38 -0500

    actually use map instead of iterating

--- src/vpn.js
+++ src/vpn.js
@@ -5,19 +5,18 @@ const handleRequest = (request) => {
   if (request.url.includes("favicon")) return new Response("",{ status: 404})
   // get connecting IP and country
   const ip = request.headers.get("CF-Connecting-IP")
-  const country = (request.cf || {}).country
+  const country = request.cf?.country
   // check if under api path
   const sendResponse = request.url.includes("/api") ? textResponse : htmlResponse
 
   // loop through map and check for IP
-  for (let [k, server] of ipmap) {
-    if (ip.startsWith(k))
-      return request.url.endsWith(".json")
-        ? jsonResponse({ip, server, country})
-        : sendResponse(`IP: ${ip}\nServer: ${server}\nCountry: ${country}`)
-  }
-  // IP not in list, not connected
-  return sendResponse(`Not Connected\nIP: ${ip}`)
+  const server = ipmap.get(ip)
+  return (server)
+    ? request.url.endsWith(".json")
+      ? jsonResponse({ip, server, country})
+      : sendResponse(`IP: ${ip}\nServer: ${server}\nCountry: ${country}`)
+    // ip not in list, not connected
+    : sendResponse(`Not Connected\nIP: ${ip}\nCountry: ${country}`)
 }
 
 const noCache = {"Cache-Control": "no-store"}
@@ -26,5 +25,5 @@ const jsonResponse = (res) => new Response(JSON.stringify(res),{headers:{...noCa
 const textResponse = (str) => new Response(str,{headers:{...noCache,"Content-Type":"text/plain"}});
 
 const htmlResponse = (ip) => new Response(
-  `<!DOCTYPE html><body style=background:#111;color:#ddd;font-size:6em;display:flex;min-height:98vh;justify-content:center;align-items:center><pre>${ip}`,
+  `<!DOCTYPE html><body style=background:#111;color:#ddd;font-size:4em;display:flex;min-height:98vh;justify-content:center;align-items:center><pre>${ip}`,
   {headers: {...noCache, "Content-Type": "text/html"}})
